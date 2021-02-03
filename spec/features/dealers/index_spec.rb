677171085a4ab3bceccb299fdda0484b24bb4a3d
Kris Litman <duguy8@gmail.com>
2021-02-02 19:04:21 -0500

    Created test for dealer index page

---
+++ spec/features/dealers/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'When I visit "/dealers"' do
+    it 'I see every dealer' do
+      dealer1 = Dealer.create(
+        id: 1,
+        name: 'Guitar Center'
+      )
+      dealer2 = Dealer.create(
+        id: 2,
+        name: 'Sam Ash'
+      )
+      dealer3 = Dealer.create(
+        id: 3,
+        name: 'Pianos n stuff'
+      )
+
+      visit '/dealers'
+      # save_and_open_page
+      expect(page).to have_content(school_1.name)
+      expect(page).to have_content(school_2.name)
+      expect(page).to have_content(school_3.name)
+    end
+  end
+end
