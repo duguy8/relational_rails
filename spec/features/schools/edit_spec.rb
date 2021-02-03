310d22675c8920ba956fc0e6aea9b3446ee18b9a
Angel Breaux <abreaux@detaso.com>
2021-02-03 14:58:10 -0600

    Remove unnecessary comments

--- spec/features/schools/edit_spec.rb
+++ spec/features/schools/edit_spec.rb
@@ -57,8 +57,3 @@ RSpec.describe 'As a visitor' do
     end
   end
 end
-
-
-# Then a `PATCH` request is sent to '/parents/:id',
-# the parent's info is updated,
-# and I am redirected to the Parent's Show page where I see the parent's updated info
