b211eab566bc110fb5195b9720d9d5f4649b44ab
Kris Litman <duguy8@gmail.com>
2021-02-02 19:27:10 -0500

    Updated school show to show all attributes

--- spec/features/schools/show_spec.rb
+++ spec/features/schools/show_spec.rb
@@ -6,13 +6,26 @@ RSpec.describe 'As a visitor' do
       school_1 = School.create(
         id: 1,
         name: 'Turing',
-        city: 'Denver'
+        address: '111 whatever st',
+        city: 'Denver',
+        state: 'CO',
+        zipcode: '80223',
+        gradeschool: false,
+        created_at: "10-10-2013",
+        updated_at: "2-2-2021"
       )
 
       visit "/schools/#{school_1.id}"
       expect(school_1.id).to eq 1
-      expect(page).to have_content(school_1.city)
       expect(school_1.city).to eq 'Denver'
+      expect(page).to have_content(school_1.name)
+      expect(page).to have_content(school_1.address)
+      expect(page).to have_content(school_1.city)
+      expect(page).to have_content(school_1.state)
+      expect(page).to have_content(school_1.zipcode)
+      expect(page).to have_content(school_1.gradeschool)
+      expect(page).to have_content(school_1.created_at)
+      expect(page).to have_content(school_1.updated_at)
     end
   end
 end
