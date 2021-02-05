619513fbff95df2bea281bbd4298ea4dcf837a2c
Kris Litman <duguy8@gmail.com>
2021-02-05 13:46:56 -0500

    Added model tests for class methods

--- spec/models/dealer_spec.rb
+++ spec/models/dealer_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Dealer, type: :model do
     describe 'relationships' do
       it {should have_many :instruments}
     end
+
+    describe 'class methods' do
+      it 'Orders dealers by recently created first' do
+        dealer1 = create(:dealer, created_at: "08-01-1983")
+        dealer2 = create(:dealer, created_at: "11-01-1990")
+        dealer3 = create(:dealer, created_at: "01-01-2020")
+
+        dealers = dealers = Dealer.all
+        expected = [dealer3, dealer2, dealer1]
+
+        expect(dealers.order_by).to eq(expected)
+      end
+    end
 end
--- spec/models/instrument_spec.rb
+++ spec/models/instrument_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe Instrument, type: :model do
     describe 'relationships' do
       it {should belong_to :dealer}
     end
+
+    describe 'class methods' do
+      it 'Finds all instruments on sale' do
+        dealer = create(:dealer, id: 1)
+        guitar1 = create(:instrument, dealer_id: 1, on_sale: true)
+        guitar2 = create(:instrument, dealer_id: 1, on_sale: false)
+        guitar3 = create(:instrument, dealer_id: 1, on_sale: true)
+
+        expected = dealer.instruments.all_on_sale
+        expect(expected.count).to eq 2
+        expect(expected[0].on_sale).to eq(true)
+        expect(expected[1].on_sale).to eq(true)
+      end
+
+      it 'Filters price to be greater than given amount' do
+        dealer = create(:dealer, id: 1)
+        guitar1 = create(:instrument, dealer_id: 1, price: 10000)
+        guitar2 = create(:instrument, dealer_id: 1, price: 500)
+        guitar3 = create(:instrument, dealer_id: 1, price: 7500)
+        guitar4 = create(:instrument, dealer_id: 1, price: 100)
+
+        expected = dealer.instruments.filter_price(200)
+        expect(expected.count).to eq 3
+        expect(expected[0].price).to eq(10000)
+        expect(expected[1].price).to eq(500)
+        expect(expected[2].price).to eq(7500)
+      end
+    end
 end
